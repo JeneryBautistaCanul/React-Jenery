21fbd55d0c1cca88f4ac87a92645f8c59b4188ca
I'm Jenery <jenerybautistacanul@gmail.com>
2023-12-04 22:25:47 -0600

    Add files via upload

---
+++ React/Proyecto-gif-Jenery/Proyecto-gif-Jenery/src/ProyectoGif.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const ProyectoGif = () => {
+
+  const [categories, setCategories] = useState(["yugioh"]);
+  // Definimos una función onAddCategory que toma un argumento newCategory
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+  };
+
+  // Devolvemos el JSX que se renderizará
+  return (
+    <>
+     <h1>Proyecto Gif Jenery</h1>
+      {/* // Renderizamos el componente AddCategory y le pasamos una función que se activará cuando se agregue una nueva categoría */}
+        <AddCategory onNewCategory={(value) => onAddCategory(value)} />
+      {/* // Mapeamos las categorías a componentes GifGrid, cada uno con una clave única y una categoría */}
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
